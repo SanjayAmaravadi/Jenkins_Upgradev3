70eccbb10e66449d569b3609e4b9d9833ba29462
SanjayAmaravadi <sanjayamaravadisj@gmail.com>
2025-12-25 08:47:36 +0530

    New test case added in TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
-  
+
+  @Test
+  public void intentionalFail(){
+    assertTrue(false);
+  }
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
